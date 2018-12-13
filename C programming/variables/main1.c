d0df84c5fd3119592324e97784348a28cbca5d18
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-13 23:18:23 +0530

    About variables

---
+++ C programming/variables/main1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// variable is place holders FOR SOMETHING ELSE ( IS THAT SIMPLE ! YES IT IS. :) )
+// in more technical language it is memory location OF bunch of data-types where actual data is stored.
+
+
+   // RULES
+    // you have to start variable name with UPPERCASE or LOWERCASE , no NUMBERS first.
+    // NO SPACING
+    // NO SYMBOLS OTHER THAN UNDERSCORES
+    // Don't name a variable any name of functions
+
+int main()
+{
+    //specify what kind of data you want to store in ( integer , float , strings )
+    int age;
+    // assign value ( = ) is use for assign values
+    age=21;// you can also do mathematical operations here (try "age=2018-1998")
+    printf("Dhruv is %d years old",age);
+    //here age is variable , so it is not in between quotations.
+    // Now you know what6 we do with converging characters.
+    return 0;
+}
