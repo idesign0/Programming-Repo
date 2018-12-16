cb8982440d7cc75a4be54539fa7666eaf3d91e3d
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-16 21:42:41 +0530

    Numbers

---
+++ Python/Numbers/Numbers.py
@@ -0,0 +1,48 @@
+
+
+#lets talk about numbers
+#And in python you have to put one hashtag on starting of the line for comments
+# Usually you open python shell ( IDLE )
+# to open this file you Should go to FILE > New , and you are in.
+# IDLE cant save your program but you can save code in "New files".
+
+
+# now lets talk about Numbers .
+# you are able to do simple math in python without declaring some variable.
+
+
+#EX
+# i recommend to try this operations on python shell
+#just tYPE IT AND press enter
+
+# i am puting one screen shot so you can understand well
+
+
+#3+3
+#3-3
+#3/3
+#3*3
+
+
+#there are some special cases
+
+#if you devide 5/3
+
+#you will get 1.66666
+
+     #if you try 5//3
+
+     # you will get only whole number
+
+     # 1
+
+# if you try 5%3
+
+# you will get reminder
+
+# 2
+
+#i've posted screenshot of output in file. 
+# and if you have some douts regarding this or any problem in future
+# mail me on " dhruvpatel2991998@gmail.com
+
