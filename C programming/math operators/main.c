c10f7264dd51aa4d778285e1c9c447050e10b16d
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-21 00:03:34 +0530

    MAIN.C update

--- C programming/math operators/main.c
+++ C programming/math operators/main.c
@@ -6,7 +6,14 @@
 int main()
 {
     // starting with initializing and declaring variable
-    int weight = 52 //
+    int weight = 52 ;
     printf("My weight is %d kg\n",weight);
+
+    // now what i want you too see guys that you can do math operations on printf statement.
+    // and values will be changed in output , see in screenshoT PAGE
+
+    printf("My weight is %d kg\n",weight + 12);
+    printf("My weight is %d kg\n",weight - 12);
+    printf("My weight is %d kg\n",weight / 12);
     return 0;
 }
