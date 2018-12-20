ec03996cf686578e82ce3bd67179fa9d66d910e9
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-20 23:59:02 +0530

    Update main.c

--- C programming/math operators/main.c
+++ C programming/math operators/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// to we are going to talk about Math operators in c
+
 int main()
 {
-    printf("Hello world!\n");
+    // starting with initializing and declaring variable
+    int weight = 52 //
+    printf("My weight is %d kg\n",weight);
     return 0;
 }
