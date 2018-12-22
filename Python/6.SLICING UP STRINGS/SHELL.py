53f8e8d8389912cd75c2ce5762bc8b7cf8b44506
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-23 01:21:11 +0530

    SHELL

---
+++ Python/6.SLICING UP STRINGS/SHELL.py
@@ -0,0 +1,44 @@
+Python 3.7.2rc1 (tags/v3.7.2rc1:75a402a217, Dec 11 2018, 23:05:39) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/dhruv/Desktop/dhruv.github.io/Python/6.SLICING UP STRINGS/6.SLICING UP STRINGS.py 
+>>> myname = ' Dhruv patel '
+>>> myname[0]
+' '
+>>> myname[5]
+'v'
+>>> myname[2]
+'h'
+>>> myname[8]
+'a'
+>>> myname[6]
+' '
+>>> myname[10]
+'e'
+>>> myname[7]
+'p'
+>>> myname[2:9]
+'hruv pa'
+>>> myname[2:5]
+'hru'
+>>> myname[2:12]
+'hruv patel'
+>>> myname[-2:-9]
+''
+>>> myname[-1:-5]
+''
+>>> myname[-7:-2]
+' pate'
+>>> myname[2:]
+'hruv patel '
+>>> myname[:9]
+' Dhruv pa'
+>>> myname[-2:]
+'l '
+>>> myname[:-9]
+' Dhr'
+>>> 
+>>> 
+>>> myname[:]
+' Dhruv patel '
+>>> 
