9cfd745c3f0c225a4965561aef046ec820e31619
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-23 00:50:58 +0530

    Create 6.SLICING UP STRINGS.py

---
+++ Python/6.SLICING UP STRINGS/6.SLICING UP STRINGS.py
@@ -0,0 +1,26 @@
+#YOU CAN HAVE ACCESS TO INDIVIDUAL CHARACTERS FROM STRINGS
+
+# LETS SEE HOW ITS WORKS
+
+#just see screenshot 1
+
+# so how it works
+#EVERY CHARACTER IN STRING HAVE ONE POSITION , STARTING FROM '0' ( COMPUTER COUNTS FROM ZERO )
+
+# > 0 (positions) WHite space also counts
+#D> 1
+#H> 2
+#R> 3
+#U> 4 
+#C> 5
+# > 6
+#P> 7
+#A> 8 
+#T> 9
+#E> 10 
+#L> 11
+# > 12
+
+# TO ACCESS CHARACTERS TYPE VARIABLE NAME AND WRITE POSITION
+# myname[5]
+# Character 'v' will show up
