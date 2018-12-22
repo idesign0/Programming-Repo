d0d0b1fdc3b9224c51f4a83d272251eba9bfb5f3
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-23 00:00:15 +0530

    Update print function.py

---
+++ Python/5.PRINT FUNCTION/print function.py
@@ -0,0 +1,22 @@
+#print is function when we want to print something on output
+
+print("My name is Dhruv")
+#You will notice something strange if you try to print any directory
+
+print("C:\Users\dhruv\Desktop\dhruv.github.io") 
+
+
+#Yes unicodeescape error
+# Remember i told about escape character on previous tutorial
+# yes it causing problems
+
+# now place "r" in starting of sentence
+
+print(r"C:\Users\dhruv\Desktop\dhruv.github.io") 
+
+
+#yes it is printed
+
+# what what r means ?
+
+# it means that " take the string as it , take no special meaning in this perticular scene "
