8f2f9dbf1e3330d272194e982de8169a3ac78e86
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-23 01:13:06 +0530

    Update 6.SLICING UP STRINGS.py

--- Python/6.SLICING UP STRINGS/6.SLICING UP STRINGS.py
+++ Python/6.SLICING UP STRINGS/6.SLICING UP STRINGS.py
@@ -1,3 +1,6 @@
+
+#Topic : 1
+
 #YOU CAN HAVE ACCESS TO INDIVIDUAL CHARACTERS FROM STRINGS
 
 # LETS SEE HOW ITS WORKS
@@ -7,20 +10,46 @@
 # so how it works
 #EVERY CHARACTER IN STRING HAVE ONE POSITION , STARTING FROM '0' ( COMPUTER COUNTS FROM ZERO )
 
-# > 0 (positions) WHite space also counts
-#D> 1
-#H> 2
-#R> 3
-#U> 4 
-#C> 5
-# > 6
-#P> 7
-#A> 8 
-#T> 9
-#E> 10 
-#L> 11
-# > 12
+# > 0 , -13 (positions) WHite space also counts
+#D> 1 , -12
+#H> 2 , -11
+#R> 3 , -10
+#U> 4 , -9 
+#C> 5 , -8
+# > 6 , -7
+#P> 7 , -6
+#A> 8 , -5 
+#T> 9 , -4
+#E> 10 , -3 
+#L> 11 , -2 
+# > 12 , -1
+
+#YOU CAN SEE THAT EACH ALPHABET HAS TWO POSITION NUMBERS
 
 # TO ACCESS CHARACTERS TYPE VARIABLE NAME AND WRITE POSITION
 # myname[5]
 # Character 'v' will show up
+
+
+# Topic : 2
+
+# Slicing string
+
+# you can slice bunch of characters from string
+
+# myname[2:9]
+
+#it means youcan take character from position 2 UPTO POSITION 9 , NOTE IT WON'T INCLUDE
+# ALPHABET OF POSITION 9  >  T , TRY IT
+
+#myname[2:]
+
+#MEANS THAT ALL CHARACTERS FROM POSITION 2 COUNTS
+
+#myname[:9]
+
+#MEANS ALPHABETS SHOWS UPTO POSITION 9 ONLY
+
+
+
+#### YOU CAN SEE IN SCREENSHOT 2 SOME TIMES STRINGS DO NOT SHOW UP , IT IS BECAUSE OF WRONG ORDER.
