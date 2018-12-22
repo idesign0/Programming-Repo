2fe8edae179363d704c696d489ae178f7c6caf14
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-22 23:24:56 +0530

    Create ABOUT STRINGS.py

---
+++ Python/4.ABOUT STRINGS/ABOUT STRINGS.py
@@ -0,0 +1,18 @@
+#strings in a computer are simply set of words of simply words
+#Example - Names , Sentences
+
+#they are written between duble quotation marks like in many programming
+# languages or you can also write between single quotations too in python generally
+
+# ex "MY NAME IS DHRUV PATEL" OR 'MY NAME IS DHRUV PATEL' ( try it in IDLE )
+
+
+# One thing is necessary to mention is that you have  be careful while using single quotation
+
+# Normally quotation marks tells pc where strings Start/End if you use it in sentence like
+# ' I don't think that ... ' ( try it on shell(idle) ) yyou will see different results
+
+
+#you can also watch in screenshot attached " SyntaxError: invalid syntax " , something like that
+
+# so i prefer to use double quotation always
