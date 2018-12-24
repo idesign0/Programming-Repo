1980e1d7d213086edecf7e298d7b291b943b2e85
Dhruv Patel <dhruvpatel2991998@gmail.com>
2018-12-24 23:54:11 +0530

    Create list.py

---
+++ Python/7.LISTS/list.py
@@ -0,0 +1,36 @@
+# Today we are going to learn al=bout lists
+# lists in python is pretty much as list ofwords or numbers,
+# lets take about Numbers
+
+
+#ex
+
+#numbers = [20,54,869,75,15,31]
+
+
+
+#you can access individual items in list , just like we did it in strings
+
+#numbers[2]
+
+#869
+
+#you can different values in perticular list itema
+
+#number[2]=25
+
+#numbers = [20,54,25,75,15,31]
+
+
+#you can add new list item to the available list
+
+#numbers + [90,55,25]
+
+#numbers
+
+#numbers = [20,54,869,75,15,31,90,55,25]
+
+
+### but this method do it tem
+
+
