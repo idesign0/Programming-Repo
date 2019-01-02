4379cfe0b8b580e8586a58976380266d29a55e3e
Dhruv Patel <dhruvpatel2991998@gmail.com>
2019-01-03 00:49:30 +0530

    break

---
+++ C programming/26.BREAK STATEMENT/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a ;
+    for(a=0;a<=100;a++)
+    {
+        printf("Print number a : %d\n",a);
+
+        if(a==22){
+                break;
+                }
+    }
+    return 0;
+}
