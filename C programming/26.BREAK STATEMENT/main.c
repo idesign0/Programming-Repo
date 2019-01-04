2029dee030f83ef52ef1c27854ca1bc1833bc94e
Dhruv Patel <dhruvpatel2991998@gmail.com>
2019-01-04 21:28:35 +0530

    changes

--- C programming/26.BREAK STATEMENT/main.c
+++ C programming/26.BREAK STATEMENT/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// now we are going to learn about break statement ,
+
+    /*
+     when we had a situation when we get our desired value in code , and don't want to proceed in program more.
+
+     let me explain.
+
+     if i have 100 numbers and then i told program give me number 76. and only 76 is my desire value , and i dont want to run program more.
+
+            in this situation i like to use break statement , if i git my value , it will break th code as name suggest and loop starts from top.
+
+        syntax;
+        for(){
+
+            if(condition){
+                break;
+            }
+        }
+
+    it it little difficult to grasp this , take time and try it own.
+     */
+
 int main()
 {
     int a ;
--- C programming/27.CONTINUE/main.c
+++ C programming/27.CONTINUE/main.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// continue is opposite of break statement , it will just live values which are written in the condition ,
+// and starts loop from TOP,
+// if you do not know know break statement's work check it out,
+/*
+syntax  // and you can use any of do while , while and for loop ,
+    do{
+        if(condition){
+           statement
+        }
+       statement
+
+    }while(condition);
+
+*/
 int main()
 {
     int a =1;
 
     do{
-        if(a==7 || a==8 ){
-            a++;
-            continue;
+        if(a==7 || a==8 ){  // here we are using or operator , you can check both values will be missing in output (7,8)
+            a++; // it is necessary you increment value before continue statement
+                    // otherwise code will be in ind=finite loop , TRy it :)
+            continue; // now what continue is doing here is it is ignoring below print statement if condition is true
         }
        printf("%d is available\n",a);
-       a++;
+       a++; // we added one value to a for further process
 
-    }while(a<=10);
+    }while(a<=10); // loop Starts from top.
 
 
     return 0;
