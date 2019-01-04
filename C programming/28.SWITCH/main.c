cd3e6b593fdaf8ed4d1c048b173a3de747e947c7
Dhruv Patel <dhruvpatel2991998@gmail.com>
2019-01-04 21:41:14 +0530

    changes

--- C programming/28.SWITCH/main.c
+++ C programming/28.SWITCH/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// switch
+
+// suppose we have bunch of conditions , and we want to test then out ,
+/*
+    but using the if else , nested if , if else if can be very handy and difficult to code ,
+
+    for simplicity we can use switch statement ,it is very simple to use .
+        let's see its syntax ;
+
+        switch(variable)
+        {
+        case ' value ' (for testing) : printf(""); //for example , main advantage is you can add multiple statement in switch cases,
+                                       break; // to exit code.
+
+                                       // you can add multiple cases below , see in example
+                                       .
+                                       .
+                                       .
+                                       .
+                                       .
+                                       .
+                                       .
+         default : printf("") // if all statements are false then one default statement executes.
+        }
+*/
+
 int main()
 {
     char grade;
